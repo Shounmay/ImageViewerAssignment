458bb8fd89fb12b4a010e97d3f7a53a23fc3de47
Shounmay <shounmaydarsh131@gmail.com>
2024-02-08 16:47:34 +0530

    v0 in client side updated with deploy

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'http://localhost:6002', // Replace with your backend base URL
+	baseURL: 'https://imageviewerserver.onrender.com', // Replace with your backend base URL
 	headers: {
 		'Content-Type': 'application/json',
 		// Add any common headers here
